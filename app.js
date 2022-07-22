2086cf5ff6de96d38fd16fb440eefc0f14fa49cc
Mark <markdejonge94@gmail.com>
2022-07-22 19:31:39 +0200

    Added workspace file and edited app.js and index.html

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("vdst");
+let originalStory = "In The Lottery by Shirley Jackson, tradition and community ties lead to deadly consequences when a woman is chosen in the eponymous lottery to be stoned to death.";
+//let originalStory = "In The Lottery by Shirley Jackson, tradition and community ties lead to deadly consequences when a woman is chosen in the eponymous lottery to be stoned to death.";
+let userInputs = ['HOI', 'DOEI', 'j'];
+
+let userStory = `In ${userInputs[0]} by ${userInputs[1]}, tradition and community ties lead to deadly consequences when a ${userInputs[2]} is chosen in the eponymous lottery to be stoned to death.`;
+
+//14.4 Part 2: Coding Instructions stap 5
+
+console.log(userStory);
+
+let favo = prompt("Hoi","k");
+
+console.log(favo);
