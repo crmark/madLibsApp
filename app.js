7850def2efbdee7fdd7ae56388f7a0d457f03fbd
Mark <markdejonge94@gmail.com>
2022-07-23 14:37:20 +0200

    added userinput with a prompt message in the for loop

--- app.js
+++ app.js
@@ -2,38 +2,34 @@
 
 let originalStory = "In The Lottery by Shirley Jackson, tradition and community ties lead to deadly consequences when a woman is chosen in the eponymous lottery to be stoned to death.";
 //let originalStory = "In The Lottery by Shirley Jackson, tradition and community ties lead to deadly consequences when a woman is chosen in the eponymous lottery to be stoned to death.";
-let userInputs = ['HOI', 'DOEI', 'j'];
+let userInputs = [];
 
 let userStory = `In ${userInputs[0]} by ${userInputs[1]}, tradition and community ties lead to deadly consequences when a ${userInputs[2]} is chosen in the eponymous lottery to be stoned to death.`;
+
 
 //14.4 Part 2: Coding Instructions stap 5
 
 let numberOfQuestions = 3;
 
-let q1 = prompt("What is your favourite store?","");
-let q2 = prompt("What is your first name?","");
-let q3 = prompt("Which pet do you have?","");
+let q1 = "What is your favourite store?";
+let q2 = "What is your first name?";
+let q3 = "Which pet do you have?";
 
 let questionArray = [q1,q2,q3];
 console.log(questionArray);
+
+
 let questionCounter = 0;
 
 for(let i = numberOfQuestions; i >= 1; i--){
     console.log(i);
-    console.log(questionArray[questionCounter] + `(${numberOfQuestions} questions left)`);
+    userInputs.push(prompt(questionArray[questionCounter] + `(${numberOfQuestions} questions left)`));
     questionCounter++;
-  //  console.log(questionArray[questionCounter] + `(${numberOfQuestions} questions left)`);
     numberOfQuestions--;
+    console.log(userInputs)
 }
 
-//console.log(questionCounter);
+alert('All done! Ready for your totally-accurate, not-at-all confusing history of tech??');
 
-
-//
-/*
-console.log(userStory);
-
-let favo = prompt("Hoi","k");
-
-console.log(favo);
-*/
+//Show the story which entered input from the user 
+//console.log(userStory)
