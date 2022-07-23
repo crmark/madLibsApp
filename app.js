32676a06fd0381a216dd5b82363c1f151cb56233
Mark <markdejonge94@gmail.com>
2022-07-23 09:26:45 +0200

    Added an array and for loop

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-console.log("vdst");
+//14.4 Part 2 Coding Instructions stap 1 t/m 4
+
 let originalStory = "In The Lottery by Shirley Jackson, tradition and community ties lead to deadly consequences when a woman is chosen in the eponymous lottery to be stoned to death.";
 //let originalStory = "In The Lottery by Shirley Jackson, tradition and community ties lead to deadly consequences when a woman is chosen in the eponymous lottery to be stoned to death.";
 let userInputs = ['HOI', 'DOEI', 'j'];
@@ -7,6 +8,19 @@ let userStory = `In ${userInputs[0]} by ${userInputs[1]}, tradition and communit
 
 //14.4 Part 2: Coding Instructions stap 5
 
+let numberOfQuestions = 3;
+
+let q1 = prompt("Q1","");
+let q2 = prompt("Q2","");
+let q3 = prompt("Q3","");
+
+let questionArray = [q1,q2,q2];
+
+for(let i = questions; i>=0; i--){
+    console.log(i);
+}
+
+//
 console.log(userStory);
 
 let favo = prompt("Hoi","k");
